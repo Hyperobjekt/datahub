7f4bf73c4b4509c0fd2578d9fd4b3754a4b65021
ubiv64 <vibu.anbarasan@gmail.com>
2021-10-27 17:23:32 -0400

    changes to CollapsibleRow component

--- src/components/CollapsibleRow.js
+++ src/components/CollapsibleRow.js
@@ -16,10 +16,20 @@ import {
 } from '@mui/icons-material'
 
 const useStyles = makeStyles({
-  
+  evenAlign: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+
+    '& > *': {
+      paddingRight: '40px'
+    }
+  }
 })
 
 const CollapsibleRow = (props) => {
+  const classes = useStyles()
+
   const [open, setOpen] = useState(false)
 
   return (
@@ -33,14 +43,25 @@ const CollapsibleRow = (props) => {
             {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
           </IconButton>
         </TableCell>
-        <TableCell></TableCell>
-        <TableCell></TableCell>
+        <TableCell>Project Name</TableCell>
+        <TableCell>
+          <Box className={classes.evenAlign}>
+            <a>9/2/21</a>
+            <a>LINK</a>
+            <a>PUBLISH</a>
+          </Box>
+        </TableCell>
       </TableRow>
       <TableRow style={{ borderBottom: 'unset', background: props.index % 2 ? '#E8F5FF' : '#FFFFFF'}}>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout='auto' unmountOnExit>
-            <Box sx={{ margin: 1, paddingLeft: 'calc(10.5%)' }}>
-              <Typography variant='h6' gutterBottom component='div'>
+            <Box sx={{ marginTop: '2px', marginBottom: '6px', marginLeft: 'calc(10.5%)', background: '#FFFFFF' }}>
+              <Typography variant='body2' gutterBottom component='div'>
+                info
+              </Typography>
+            </Box>
+            <Box sx={{ marginTop: '2px', marginBottom: '6px', marginLeft: 'calc(10.5%)', background: '#FFF8ED' }}>
+              <Typography variant='body2' gutterBottom component='div'>
                 Project Name
               </Typography>
             </Box>
