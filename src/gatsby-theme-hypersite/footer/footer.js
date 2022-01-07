1260da0c55cf754f12c62e95366ab2283790a312
ubiv64 <vibu.anbarasan@gmail.com>
2022-01-06 22:05:43 -0500

    new import statement

--- src/gatsby-theme-hypersite/footer/footer.js
+++ src/gatsby-theme-hypersite/footer/footer.js
@@ -1,5 +1,20 @@
-import { Footer as BaseFooter} from 'gatsby-theme-hypersite/src/footer'
+import React from 'react'
+
+import { Footer as BaseFooter} from '@hyperobjekt/material-ui-website'
+
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+
+})
+
+const TextFooter = () => { return (<div>vibu</div>) }
 
 export default function Footer(props) {
-  return null
+  const classes = useStyles()
+
+  return (
+    <BaseFooter style={{ height: '700px', background: "green", padding: `24px 0` }} {...props}>
+    </BaseFooter>
+  )
 }
