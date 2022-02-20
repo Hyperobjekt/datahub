40d480ac0b896c4f97edbe975cb857f8d22cd14d
anbav <vibu.anbarasan@gmail.com>
2022-02-19 19:21:59 -0500

    About Us page

--- src/components/about/AboutUs.js
+++ src/components/about/AboutUs.js
@@ -7,31 +7,60 @@ import {
 
 import { Block, Hero } from '@hyperobjekt/material-ui-website'
 import { AboutUsPage } from './styles/AboutStyles'
+import HereLabLogo from '../../../static/icons/hereLabLogo.svg'
+import BlueBox from '../general/BlueBox';
 import OurTeam from './OurTeam'
 
 import aboutUsObj from '../../../content/data/about.json'
 
-const HighlightedText = (highlight, lede) => {
+const HighlightedText = (hlText, hlBox, highlight, lede) => {
   
   return (
-    <>
-      <Box>
-        <Typography>
+    <Box className={hlText}>
+      <Box className={hlBox}>
+        <Typography variant="h1" className="bold">
           {highlight}
         </Typography>
       </Box>
-      <Typography>
+      <Typography variant="body1">
         {lede}
       </Typography>
-    </>
+    </Box>
   )
 }
 
 const AboutUs = () => {
   const classes = AboutUsPage()
 
+  const firstBlock = (firstBlock) => {
+    const highlight = firstBlock.motive, lede = firstBlock.location
+
+    return HighlightedText(classes.hlText, classes.hlBox, highlight, lede)
+  }
+
+  const secondBlock = (secondBlock) => {
+    const title = <Typography variant="overline">WHAT DRIVES US TO DO THIS WORK?</Typography>
+    const copy1 = <Typography variant="body2" className="bold">{secondBlock.drive}</Typography>
+    const copy2 = <Typography variant="body2">{secondBlock.approach}</Typography>
+
+    return (
+      <BlueBox
+        itemStyles={classes.itemStyles}
+        copyStyles={classes.copyStyles}
+        title={title}
+        copy1={copy1}
+        copy2={copy2}
+      /> 
+    )
+  }
+
+  const thirdBlock = (thirdBlock) => {
+    const highlight = thirdBlock.motive, lede = thirdBlock.approach
+
+    return HighlightedText(classes.hlText, classes.hlBox, highlight, lede)
+  }
+
   return (
-    console.log(aboutUsObj),
     <>
       <Hero
         ContainerProps={{
@@ -46,9 +75,19 @@ const AboutUs = () => {
 
       <Block>
         <Box className={classes.aboutUs}>
-          <Typography variant='h5'>
-              ABOUT US
-          </Typography>
+          <Box>
+            <Typography variant='h5'>
+                ABOUT US
+            </Typography>
+            {firstBlock(aboutUsObj.firstBlock[0])}
+          </Box>
+          {secondBlock(aboutUsObj.secondBlock[0])}
+          {thirdBlock(aboutUsObj.thirdBlock[0])}
+          <Box className={classes.logo}>
+            <img src={HereLabLogo} alt={'Here Lab logo'} />
+            <Typography variant="body1">{aboutUsObj.fourthBlock[0].positionOne}</Typography>
+            <Typography variant="body1">{aboutUsObj.fourthBlock[0].positionTwo}</Typography>
+          </Box>
         </Box>
       </Block>
     </>
--- src/components/about/styles/AboutStyles.js
+++ src/components/about/styles/AboutStyles.js
@@ -2,9 +2,50 @@ import { makeStyles } from "@material-ui/core"
 
 export const AboutUsPage = makeStyles(theme => ({
   aboutUs: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
     margin: '0 auto',
     [theme.breakpoints.down('sm')]: {
       margin: '0',
     }
+  },
+  itemStyles: {
+    backgroundColor: '#E8F5FF',
+
+    maxWidth: theme.spacing(90),
+    padding: theme.spacing(3),
+    margin: '20px 0 40px 0'
+  },
+  copyStyles: {
+    display: 'flex',
+    flexDirection: 'column',
+
+    padding: theme.spacing(3),
+
+    '& > :first-child': {
+      textAlign: 'center'
+    },
+    '& > *': {
+      paddingBottom: theme.spacing(2)
+    },
+  },
+  hlText: {
+    maxWidth: theme.spacing(70),
+    marginTop: theme.spacing(3)
+  },
+  hlBox: {
+    width: '587px',
+    paddingBottom: theme.spacing(3),
+    borderBottom: '.3em solid'
+  },
+  logo: {
+    width: '587px',
+    margin: '50px 0 0 0',
+
+    '& > *': {
+      paddingBottom: '15px'
+    },
   }
 }))
