e0c3061dcbf757336348d8f063b6078a90a00adf
lane <laneolson@gmail.com>
2022-03-03 12:56:17 -0800

    fix: no auto scroll down homepage when navigating via logo

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,53 +1,40 @@
-import React, { useEffect } from 'react';
-
-import { 
-  Box
-} from '@material-ui/core';
-
+import React from 'react';
 import { Block, Hero } from '@hyperobjekt/material-ui-website';
 import ResourcesTable from './ResourcesTable';
 import SplitHeader from './SplitHeader';
 import RoundedButton from '../general/RoundedButton';
 import { HomePageStyles } from './styles/HomeStyles';
-
-import HomeHero from "../../../static/graphics/heroHeaderStatic.png"
 import HeroText from '../general/HeroText';
 
 const handleScrollClick = () => {
-  const scrollTo = document.getElementById('scrollTo')
-
-  scrollTo.scrollIntoView()
-}
+  const scrollTo = document.getElementById('scrollTo');
+  scrollTo.scrollIntoView();
+};
 
 const HomePage = () => {
-  const classes = HomePageStyles()
-
-  useEffect(() => {
-    const homePage = document.getElementById('heroScrollTo')
-
-    homePage.scrollIntoView()
-  }, [])
+  const classes = HomePageStyles();
 
   return (
     <div id="heroScrollTo" className={classes.backgroundDiv}>
       <div id="homeHero">
         <Hero
-          id="heroScrollTo"
           ContainerProps={{
             justifyContent: 'flex-start',
           }}
           className={classes.hero}
         >
           <HeroText
-            heading={"THE DATA HUB AT UC MERCED"}
-            subheading={"We publish code for linking organizational and socio-economic data for thousands of US colleges."}
+            heading={'THE DATA HUB AT UC MERCED'}
+            subheading={
+              'We publish code for linking organizational and socio-economic data for thousands of US colleges.'
+            }
           />
           <RoundedButton
             handleClick={handleScrollClick}
             text={'BROWSE OUR DATA AND CODE'}
           />
         </Hero>
-        </div>
+      </div>
       <SplitHeader />
 
       <Block>
